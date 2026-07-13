afef86ae235b726c9697ce65a8bbdd3405b2a86e
Guillem <guillemarcos23@gmail.com>
2026-07-13 11:16:20 +0200

    Remove home from outer navigation stack

--- ios/Blank/Blank/ContentView.swift
+++ ios/Blank/Blank/ContentView.swift
@@ -4,13 +4,11 @@ struct ContentView: View {
     @EnvironmentObject private var sessionStore: SessionStore
 
     var body: some View {
-        NavigationStack {
-            Group {
-                if sessionStore.setupComplete {
-                    HomeView()
-                } else {
-                    SetupView()
-                }
+        Group {
+            if sessionStore.setupComplete {
+                HomeView()
+            } else {
+                SetupView()
             }
         }
     }

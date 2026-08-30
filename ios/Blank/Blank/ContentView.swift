6d2b5d4aa051c17e28043ddb446575006de3f193
Guillem <guillemarcos23@gmail.com>
2026-08-30 21:13:18 +0200

    Launch conversational home by default

--- ios/Blank/Blank/ContentView.swift
+++ ios/Blank/Blank/ContentView.swift
@@ -12,16 +12,8 @@ struct ContentView: View {
     @EnvironmentObject private var sessionStore: SessionStore
 
     var body: some View {
-        ZStack {
-            if sessionStore.setupComplete {
-                ConversationalHomeView()
-                    .transition(.opacity.combined(with: .scale(scale: 1.01, anchor: .center)))
-            } else {
-                SetupView()
-                    .transition(.opacity)
-            }
-        }
-        .animation(.easeInOut(duration: 0.85), value: sessionStore.setupComplete)
+        ConversationalHomeView()
+            .transition(.opacity.combined(with: .scale(scale: 1.01, anchor: .center)))
     }
 }
 
